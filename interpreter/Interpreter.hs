437286c88ecc1e6efaf635b76279eb4e391c7f4d
Asia <jw429677@students.mimuw.edu.pl>
2023-05-07 20:08:22 +0200

    everything is compiling :)

--- interpreter/Interpreter.hs
+++ interpreter/Interpreter.hs
@@ -10,7 +10,106 @@ module ExecStmt where
 
     import Types
     import Memory
-    import EvalExpr
+
+-- EXPRESSION --
+    evalAddOp :: AddOp -> Integer -> Integer -> Integer
+    evalAddOp Minus e1 e2 = e1 - e2
+    evalAddOp Plus e1 e2 = e1 + e2
+
+    evalMulOp :: MulOp -> Integer -> Integer -> Integer
+    evalMulOp Mul e1 e2 = e1 * e2
+    evalMulOp Div e1 e2 = div e1 e2
+    evalMulOp Mod e1 e2 = e1 `mod` e2
+
+    evalRelOp :: RelOp -> Integer -> Integer -> Bool
+    evalRelOp LTH e1 e2 = e1 < e2
+    evalRelOp LE e1 e2 = e1 <= e2
+    evalRelOp GTH e1 e2 = e1 > e2
+    evalRelOp GE e1 e2 = e1 >= e2
+    evalRelOp Grammar.Abs.EQ e1 e2 = e1 == e2
+    evalRelOp NEQ e1 e2 = e1 /= e2
+
+    evalExpr :: Expr -> Interpreter VMemory
+    evalExpr (EVar ident) = getValueFromIdent ident
+    evalExpr ETrue = return $ VBool True
+    evalExpr EFalse = return $ VBool False
+    evalExpr (EInt var) = return $ VInt var
+    evalExpr (EString str) = return $ VString str
+    evalExpr (EEmptyList typ) = return $ VList (typ, [])
+
+-- bool operators
+    evalExpr (ERel e1 op e2) = do
+        VInt done1 <- evalExpr e1 
+        VInt done2 <- evalExpr e2
+        return $ VBool $ evalRelOp op done1 done2
+
+    evalExpr (EAnd e1 e2) = do
+        VBool done1 <- evalExpr e1 
+        VBool done2 <- evalExpr e2
+        return $ VBool $ done1 && done2
+
+    evalExpr (EOr e1 e2) = do
+        VBool done1 <- evalExpr e1 
+        VBool done2 <- evalExpr e2
+        return $ VBool $ done1 || done2
+
+    evalExpr (ENot e) = do
+        VBool done <- evalExpr e
+        return $ VBool $ not done
+    
+--  int operators
+    evalExpr (EAdd e1 op e2) = do
+        VInt done1 <- evalExpr e1 
+        VInt done2 <- evalExpr e2
+        return $ VInt $ evalAddOp op done1 done2
+
+    evalExpr (ENeg e) = do
+        VInt done <- evalExpr e 
+        return $ VInt $ -done
+
+    evalExpr (EMul e1 op e2) = do
+        VInt done1 <- evalExpr e1 
+        VInt done2 <- evalExpr e2
+        if done2 == 0 && (op == Div || op == Mod) then 
+            throwError ZeroDivisionException 
+        else 
+            return $ VInt $ evalMulOp op done1 done2
+
+-- lists
+    evalExpr (EListLen l) = do
+        VList (_, elements) <- evalExpr l
+        return $ VInt $ fromIntegral $ length elements
+
+    evalExpr (EListAt l pos) = do
+        VInt idx <- evalExpr pos
+        VList (_, elements) <- evalExpr l
+        let listLen = fromIntegral (length elements)
+        if idx < 0 || listLen <= idx then
+            throwError $ OutOfRangeExeption listLen idx
+        else 
+            return $ elements !! fromIntegral idx
+
+    evalExpr (EApp ident vars) = do
+        VFun fun <- evalExpr ident
+        values <- mapM evalVarForFun vars
+        fun values
+--lambda
+    evalExpr (ELambda args typ (Block block)) = do 
+        fun <- makeFun args block typ
+        return $ VFun fun
+
+    evalVarForFun :: ExprOrRef -> Interpreter FuncVal
+    evalVarForFun (EorRRef ident) = do
+        return $ ByReference ident
+
+    evalVarForFun (EorRExpr expr) = do
+        val <- evalExpr expr
+        return $ ByType val
+
+
+
+-- structs
+
 
     
 -- Declarations
